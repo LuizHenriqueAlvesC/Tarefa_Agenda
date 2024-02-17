fb0e8ff222b5de1e1c6a29465f2bebb2cff41601
luiz980alves@gmail.com <luiz980alves@gmail.com>
2024-02-17 13:09:01 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form-atividade');
+const nomeCompleto = [];
+const telefone = [];
+
+let linhas ='';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+})
+
+function adicionalinha() {
+    const inputNomeCompleto = document.getElementById('nome-completo');
+    const inputTelefone = document.getElementById('telefone');
+
+    if (nomeCompleto.includes(inputNomeCompleto.value)) {
+        alert(`Os dados:${inputNomeCompleto.value} - ${inputTelefone.value} já foram inseridos`)
+    }else {
+        nomeCompleto.push(inputNomeCompleto.value);
+        telefone.push(inputTelefone.value);
+
+        let linha = `<tr>`;
+        linha += `<td>${inputNomeCompleto.value}</td>`;
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += `</tr>`;
+
+        linhas += linha;
+    }
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
